fdf4202dcee28ddc7bac95b9333306170801dcda
ilya <shalaginovilya703@gmail.com>
2022-10-19 19:58:31 +0300

    Server logger

--- server/src/main.ts
+++ server/src/main.ts
@@ -7,7 +7,9 @@ async function bootstrap() {
 
   const PORT = process.env.PORT || 4999
 
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    logger: ['error', 'warn'],
+  });
 
   const config = new DocumentBuilder()
       .setTitle('Shop API')
