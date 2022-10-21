ed0c4ae2134b33c492557c31022ef154e8d0eb2e
ilya <shalaginovilya703@gmail.com>
2022-10-21 22:46:09 +0300

    Protecting the app from some well-known web vulnerabilities by using Helmet

--- server/src/main.ts
+++ server/src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 import {CustomValidationPipe} from "./pipe/validation.pipe";
 import {DocumentBuilder, SwaggerModule} from "@nestjs/swagger";
 import * as compression from 'compression';
+import helmet from "helmet";
 
 async function bootstrap() {
 
@@ -11,6 +12,11 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     logger: ['error', 'warn'],
   });
+
+  app.use(helmet({
+    crossOriginEmbedderPolicy: false,
+    crossOriginResourcePolicy: false,
+  }));
 
   app.use(compression({
     filter: () => { return true },
